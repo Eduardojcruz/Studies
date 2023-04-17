471c4d9d7a1c3a0f454005d712d25e14ba94b9ea
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-16 21:25:57 -0300

    Aula002

---
+++ Aula002.py
@@ -0,0 +1,21 @@
+"""
+Tipo de tipagem = Dinâmica /  Forte
+Strings são textos que estão dentro de aspas
+
+Podemos utilizar aspas simples ou aspas duplas
+"""
+#Aspas simples
+print('Texto')
+
+#Aspas duplas
+print("Texto")
+
+#Metodos para aparecer aspas
+print('Programação "Básica"')
+
+#Escape
+#Utilizado para quando precisamos utilias as aspas dentro do texto
+print("Programação \"Básica\"")
+
+#r
+print(r"Programação \"Básica\"")
