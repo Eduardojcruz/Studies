e6eedf1afa6a12f63b8f962b49dd84ddbb57b01c
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-18 18:23:13 -0300

    Aula006

---
+++ Aula006.py
@@ -0,0 +1,5 @@
+nome = input("Nome:")
+altura = float(input("Altura: "))
+peso = float(input("Peso: "))
+imc = peso / altura ** 2
+print("{} tem {} de altura, pesa {} quilos e seu imc é {}".format(nome,altura,peso,imc))
