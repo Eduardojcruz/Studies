c7659965b385aff7163fb20de6ecc4dd10ea2c49
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-19 20:38:25 -0300

    Aula012

---
+++ Aula012.py
@@ -0,0 +1,18 @@
+"""
+Formatação basica de strings
+
+s = string
+d = int
+f = float
+.<número de digitos>f
+Caractere )<>^(quantidade)
+>  esquerda
+< direita
+^ Centro
+= força o número a apparecer o antes dos zeros
+
+"""
+variavel = '!EFG!'
+print(f'{variavel:#>10}')
+print(f'{variavel:#<10}')
+print(f'{variavel:#^10}')
