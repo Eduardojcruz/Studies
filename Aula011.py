d52ea8667f9595c5265160daac84ab5dae13fae1
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-19 20:25:58 -0300

    Aula011

---
+++ Aula011.py
@@ -0,0 +1,12 @@
+"""
+Interpolação básica
+s = string
+d e i = int
+f = float
+x e X = Hexadecimal
+"""
+nome = "Nome_teste"
+preco = 1923.312984129851
+variavel = '%s, o preço é R$:%.2f' % (nome,preco)
+print(variavel)
+print('O hexadecimal de %d é %04X' % (15, 15))
