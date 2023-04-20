2396f37566e70525673ef8afe415d08a4aee79fa
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-20 20:16:41 -0300

    Aula013

---
+++ Aula0013.py
@@ -0,0 +1,16 @@
+"""
+Fatiamente de strings
+
+-012345678
+-Olá mundo
+-987654321
+
+Fatimente [inicio:fim:pular]
+
+"""
+variavel = "Hello World"
+
+print(variavel[2:5])
+print(variavel[2:])
+print(variavel[-11:-4])
+
