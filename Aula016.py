09a95553502c2548d3340e1c85679029a4b6d156
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-23 21:42:22 -0300

    Aula016

--- Aula016.py
+++ Aula016.py
@@ -4,9 +4,11 @@ local_carro = 100
 RADAR_1 = 60
 LOCAL_1 = 100
 RADAR_RANGE = 1
+velocidade_carro_páss_radar_1 = velocidade > RADAR_RANGE
+carro_multado_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE)
 
-if velocidade > RADAR_1:
+if velocidade_carro_páss_radar_1:
     print("Passou do radar 1")
 
-if local_carro >= (LOCAL_1 - RADAR_RANGE) and local_carro <= (LOCAL_1 + RADAR_RANGE) and velocidade > RADAR_1:
+if carro_multado_radar_1 and velocidade_carro_páss_radar_1:
     print("Carro multado")
