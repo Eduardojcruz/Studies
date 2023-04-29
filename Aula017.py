1360c4d1b53f4a32998b60a1d5af46156e842243
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-04-29 14:45:02 -0300

    Aula017

--- Aula017.py
+++ Aula017.py
@@ -4,6 +4,18 @@ None = não valor
 is e is not = é ou não é
 
 """
-v1 = 'b'
-print(id(v1))
+#v1 = 'b'
+#print(id(v1))
 
+condicao = False
+passou_if = None
+
+if condicao:
+    print('Faça algo')
+    passou_if = True
+else: 
+    print("não faça algo")
+
+print(passou_if, passou_if is None)
+
+print(passou_if, passou_if is  not None)
