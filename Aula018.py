089126e2e857ae3f8bf0aa3f34333fc0d34fc43c
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-05-06 16:38:07 -0300

    Aula018

---
+++ Aula018.py
@@ -0,0 +1,15 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enuqanto uma condição for verdadeira
+
+"""
+contador = 1
+while True:
+    print(contador)
+    if contador == 10:
+        break
+    contador=contador + 1
+print("Acabou")
+
+    
