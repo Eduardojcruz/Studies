c38db1809f4faa12dedb4789cb8545e66727da9d
Eduardojcruz <eduardo.jose.cruz@outlook.com>
2023-05-11 20:09:03 -0300

    Aula018

--- Aula018.py
+++ Aula018.py
@@ -7,11 +7,15 @@ Executa uma ação enquanto uma condição for verdadeira
 contador = 1
 
 while True:
-    print(contador)
+    contador=contador + 1
+    
+    
     if contador == 100:
         break
-    contador=contador + 1
-
+    if contador == 10:
+        print("Não vou mostrar o 10")
+        continue
+    print(contador)
 print("Acabou")
 
 
